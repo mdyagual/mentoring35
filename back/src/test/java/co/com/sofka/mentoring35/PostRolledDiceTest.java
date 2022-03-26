b5a471a97479502544a889d9dba6573651b4c2ea
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-26 09:06:54 -0500

    Prueba unitaria del método post, pasada.

---
+++ back/src/test/java/co/com/sofka/mentoring35/PostRolledDiceTest.java
@@ -0,0 +1,30 @@
+package co.com.sofka.mentoring35;
+
+import co.com.sofka.mentoring35.model.Dado;
+import co.com.sofka.mentoring35.service.DadoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+public class PostRolledDiceTest {
+    @Autowired
+    DadoService dadoService;
+
+    @Test
+    void postTest(){
+        Dado dado = new Dado();
+        Mono<Dado> dadoMono= dadoService.saveRoll(dado);
+        StepVerifier.create(dadoMono).expectNext(dado).verifyComplete();
+
+    }
+
+    @Test
+    void getAllTest(){
+        Flux<Dado> getDadoFlux = dadoService.getAllRoll();
+        StepVerifier.create(getDadoFlux).expectComplete().verify();
+    }
+}
