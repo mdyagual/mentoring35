2efbfb0e6313744c9bdc89c8f03b3ad6f9ddb46c
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-26 02:25:04 -0500

    Implementación de dos servicios para obtener los lanzamientos almacenados y guardar los lanzamientos

---
+++ back/src/main/java/co/com/sofka/mentoring35/service/DadoService.java
@@ -0,0 +1,23 @@
+package co.com.sofka.mentoring35.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.com.sofka.mentoring35.model.Dado;
+import co.com.sofka.mentoring35.repository.DadoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class DadoService {
+    @Autowired
+    DadoRepository dadoRepo;
+
+    public Flux<Dado> getAllRoll(){
+        return dadoRepo.findAll();
+    }
+
+    public Mono<Dado> saveRoll(Dado dado){
+        return dadoRepo.save(dado);
+    }
+}
