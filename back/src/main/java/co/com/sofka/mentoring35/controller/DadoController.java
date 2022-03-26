29a8bd0dff6516fbd12639fa15f2e68764e64df8
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-26 02:29:45 -0500

    Implementación en el controlador para eliminar un lanzamiento existente

--- back/src/main/java/co/com/sofka/mentoring35/controller/DadoController.java
+++ back/src/main/java/co/com/sofka/mentoring35/controller/DadoController.java
@@ -2,7 +2,9 @@ package co.com.sofka.mentoring35.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,10 @@ public class DadoController {
     @PostMapping("/saveRoll")
     public Mono<Dado> guardarLanzamiento(@RequestBody Dado d){
         return dadoService.saveRoll(d);
+    }
+
+    @DeleteMapping("/deleteRoll/{id}")
+    public void eliminarLanzamiento(@PathVariable("id") String id){
+        dadoService.deleteRoll(id);
     }
 }
