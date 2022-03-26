1778e81ddb1b935ee6933161d63c60b5b4af4565
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-26 08:30:35 -0500

    Cambio de lugar para que carguen en la página

--- front/src/actions/index.js
+++ front/src/actions/index.js
@@ -2,20 +2,19 @@
 
 export const fetchRandom = (state) => (dispatch) => {
 
+    
     dispatch({ type: "view-loading" });
+    
 
     return fetch(`http://localhost:8080/appDado/saveRoll`, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({list: state}) // body data type must match "Content-Type" header
+        body: JSON.stringify({name: state}) // body data type must match "Content-Type" header
     }).then(response => response.json())
       .then(json => {
           dispatch({ type: "random-result", data: json });
           dispatch({ type: "view-loaded" });
         })
 }
-
-
-//TODO: agregar las demas acciones
